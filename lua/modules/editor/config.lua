1c29574f8062e6156dc7edae7fba3721cd604379
Rikka <1103670381@qq.com>
2021-11-21 11:24:09 +0800

    feat: support nasm debug

--- lua/modules/editor/config.lua
+++ lua/modules/editor/config.lua
@@ -497,6 +497,30 @@ function config.dap()
         type = 'executable',
         command = '/home/rikka/.local/share/nvim/dapinstall/ccppr_vsc/extension/debugAdapters/bin/OpenDebugAD7',
       }
+      dap.configurations.asm = {
+        {
+            name = "Launch file",
+            type = "cppdbg",
+            request = "launch",
+            program = function()
+            return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+            end,
+            cwd = '${workspaceFolder}',
+            stopOnEntry = true,
+        },
+        {
+            name = 'Attach to gdbserver :1234',
+            type = 'cppdbg',
+            request = 'launch',
+            MIMode = 'gdb',
+            miDebuggerServerAddress = 'localhost:1234',
+            miDebuggerPath = '/home/rikka/.local/share/nvim/dapinstall/ccppr_vsc/gdb-10.2/gdb',
+            cwd = '${workspaceFolder}',
+            program = function()
+            return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+            end,
+        },
+        }
     dap.configurations.cpp = {
     {
         name = "Launch file",
